07e82df5dc2e81cf4fb45dbaebcbb4060bb47aba
imprajit <prajitr24@gmail.com>
2026-02-28 20:06:45 +0530

    two program

---
+++ two.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class two{
+    public static void main(String args[]){
+     Scanner num= new Scanner(System.in);
+     System.out.println("Enter first the number:");
+     System.out.println("Enter seecond the number:");
+     int a=num.nextInt();
+     int b=num.nextInt();
+     System.out.print(a+b);
+    }
+
+}
+
