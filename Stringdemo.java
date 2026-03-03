9f9b554fafd7dfbb36178b919d30d0ad84837840
imprajit <prajitr24@gmail.com>
2026-03-03 22:34:36 +0530

    String comparison

---
+++ Stringdemo.java
@@ -0,0 +1,10 @@
+class Stringdemo{
+ public static void main(String[] args) {
+        String e1= "orange";
+      String e2= "orange";
+        String f1= new String ("apple");
+         String f2= new String ("apple");
+        System.out.println(e1==e2);
+        System.out.println(f1.equals(f2));
+    }
+}
