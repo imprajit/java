211f104c11fb79425eeab53cfdbf93b7933f7d52
imprajit <prajitr24@gmail.com>
2026-03-04 19:30:56 +0530

    seventh program

---
+++ ifelse.java
@@ -0,0 +1,13 @@
+import java.util.Scanner ;
+class ifelse {
+    public static void main(String[] args) {
+    Scanner wo = new Scanner (System.in);
+    String RCB= wo.nextLine();
+    if(RCB.equals("win")){
+        System.out.print("Ee sala cup namade");
+    }
+    else{
+        System.out.print("No cup");
+    }
+    }
+}
