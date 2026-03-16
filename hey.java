e1da40907cd429b885c1686e7388d31eb08ebca4
imprajit <prajitr24@gmail.com>
2026-03-16 22:30:18 +0530

    oddoreven

---
+++ hey.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+        int oddcount=0;
+        int evencount=0;
+        for(int i=1;i<=10;i=i+1)
+        {
+            System.out.println(i);
+        if(i %2==0)    
+          {
+              evencount=evencount+1;
+              System.out.println("Even number:"+i);
+          }
+          else
+          {
+             oddcount=oddcount+1;
+              System.out.println("Odd number:"+i);
+          }
+         System.out.println("Total odd:"+oddcount);
+         System.out.println("Total even:"+evencount);
+        }
+    }
+}
