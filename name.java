93cdb1a7efec6660366458e7882d99b457d8d598
imprajit <prajitr24@gmail.com>
2026-03-16 19:03:30 +0530

    nothing

---
+++ name.java
@@ -0,0 +1,13 @@
+class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i=i+1)
+        {
+            // System.out.print(i);
+        
+    if(i %2==0)
+    {
+        System.out.println("Even numbers:" +i);
+    }
+        }
+}
+}
