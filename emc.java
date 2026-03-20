dd117ae31398c8e3616d49622fc0194464fcfe6c
imprajit <prajitr24@gmail.com>
2026-03-20 19:12:01 +0530

    array

---
+++ emc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner (System.in);
+        int[] score = new int[5];
+       
+         for(int i=0;i<5;i=i+1)
+         {
+        score [i] = scan.nextInt();
+         }
+    }
+} 
