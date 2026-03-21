62f67db2a921cd5baba6c393985389c789f1a76b
imprajit <prajitr24@gmail.com>
2026-03-21 15:43:35 +0530

    arrayyy

---
+++ arrayandloop.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        int[] num = new int[10];
+        for(int i=0;i<=10;i=i+1)
+        {
+            System.out.println(i);
+        }
+    }
+}
---
+++ example.java
@@ -0,0 +1,9 @@
+class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i=i+1)
+        {
+          System.out.println(i+"x2="+i*2);  
+        }
+       
+    }
+}
