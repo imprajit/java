dda009961c93107164798043506b19e9d865f71d
imprajit <prajitr24@gmail.com>
2026-03-21 16:13:04 +0530

    tables

---
+++ tables.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int num=scan.nextInt();
+        int num=scan.nextInt();
+        for(int i=1;i<=10;i=i+1)
+        {
+          System.out.println()  
+        }
+       
+    }
+}
