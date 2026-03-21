ace01db31ae2228d67055d424a7e735d579a05d1
imprajit <prajitr24@gmail.com>
2026-03-21 21:47:00 +0530

    nested loop

---
+++ learning.java
@@ -0,0 +1,12 @@
+class Main {
+    public static void main(String[] args) {
+        for(int i=1;i<=3;i=i+1)
+        {
+           for(int w=1;w<=3;w=w+1)
+           {
+               System.out.print("*");
+           }
+           System.out.println();
+}
+}
+}
