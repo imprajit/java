d057486333f11919596a26e9143533cda5cf161a
imprajit <prajitr24@gmail.com>
2026-03-29 22:31:23 +0530

    learning loop

--- loopstar.java
+++ loopstar.java
@@ -1,8 +1,9 @@
+
 class Main {
     public static void main(String[] args) {
         for(int i=1;i<=3;i=i+1)
         {
-           for(int w=1;w<=3;w=w+1)
+           for(int w=1;w<=i;w=w+1)
            {
                System.out.print("*");
            }
