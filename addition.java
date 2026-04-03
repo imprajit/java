f334bb0c08eda4ea3394d62f3e4146c0726dd533
imprajit <prajitr24@gmail.com>
2026-04-03 14:07:40 +0530

    void

---
+++ addition.java
@@ -0,0 +1,13 @@
+
+public class addition{
+     int a = 10;
+     int b = 100;
+     void sum()
+     {
+        System.out.print(a+b);
+     }
+    public static void main(String args[] ){
+        addition obj1 = new addition();
+        obj1.sum();
+    }
+}
