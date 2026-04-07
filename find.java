0282aabe46d0298a672a685aea750bd860459841
imprajit <prajitr24@gmail.com>
2026-04-07 21:36:08 +0530

    0ddoreven

---
+++ find.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class find{
+     void evenorodd(int num)
+     {
+        if (num%2==0)
+        {
+            System.out.println("even number");
+        }
+        else 
+        {
+            System.out.print("odd number");
+        }
+     }
+    public static void main(String args[])
+    {
+      Scanner scan = new Scanner (System.in);
+      int num = scan.nextInt();
+      find obj1 = new find();
+      obj1.evenorodd(num);
+    }
+}
