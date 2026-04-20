f31e617619803575603c862d8132c6ef80a523a6
imprajit <prajitr24@gmail.com>
2026-04-20 16:08:09 +0530

    Final Keyword explain click here

---
+++ finalkeyword.java
@@ -0,0 +1,19 @@
+package package_one;
+
+  final class ConstantValue
+  {
+	  final float  variable = 3.14f;
+  }
+  class tryto extends  ConstantValue
+  {
+   // Here I want error to show that if you try to extend a Final keyword class or variable 
+	  // it will show Thanks for for giving me your valuable time!!.
+  }
+public class practice {
+
+	public static void main(String[] args) {
+	}
+	
+	
+}
+
